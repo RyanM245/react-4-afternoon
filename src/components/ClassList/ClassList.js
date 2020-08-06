e85528b734a19f4df5f826a8dede992b8b64a041
Ryan Milne <ryanmilne101@gmail.com>
2020-08-06 12:02:45 -0600

    Done

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import {Link} from 'react-router-dom'
 
 export default class ClassList extends Component {
   constructor() {
@@ -23,9 +24,11 @@ export default class ClassList extends Component {
 
   render() {
     const students = this.state.students.map((student, i) => (
-      <h3 key={i}>
+      <Link to = {`/student/${student.id}`} key={i}>
+      <h3>
         {student.first_name} {student.last_name}
       </h3>
+      </Link>
     ));
     return (
       <div className="box">
